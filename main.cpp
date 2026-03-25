76f15c2c7d4de8e0e258228f0494d14d12c05d94
Андрей <avp22.07@mail.ru>
2026-03-25 15:11:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
 #include "pe-vector.hpp"
 using knk::Vector
 
-bool test1()
+bool test1(const char ** pname)
 {
+  *pname = __func__;
   knk::Vector< int > v;
   return true;
 }
 
-bool test2()
+bool test2(const char ** pname)
 {
+  *pname = __func__;
   Vector< int > v;
   return v.isEmpty();
 }
 
 int main()
 {
-  using test_t = bool(*)();
+  using test_t = bool(*)(const char **);
   test_t tests[] = {
-    test1
+    test1,
+    test2
   };
   constexpr size_t count = sizeof(tests) / sizeof(test_t);
   for(size_t i = 0; i < count; ++i) {
-    bool r = tests[i]();
+    bool r = tests[i](&testName);
     if (!r) {
-      std::cout << "Failed: " << i << "\n";
+      std::cout << "Failed: " << testName << "\n";
     }
   }
 }
