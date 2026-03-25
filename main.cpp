008d66f98f4c3e175845d678e27d2b8f9892e0f4
Андрей <avp22.07@mail.ru>
2026-03-25 14:52:35 +0300

    add tests

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
-template< class T >
-struct Vector {
-  T* data;
-  size_t size, capacity;
-};
+#include "pe-vector.hpp"
+
+bool test1()
+{
+  knk::Vector< int > v;
+  return true;
+}
 
 int main()
 {
-  
+  using test_t = bool(*)();
+  test_t tests[] = {
+    test1
+  };
+  constexpr size_t count = sizeof(tests) / sizeof(test_t);
+  for(size_t i = 0; i < count; ++i) {
+    bool r = tests[i]();
+    if (!r) {
+      std::cout << "Failed: " << i << "\n";
+    }
+  }
 }
